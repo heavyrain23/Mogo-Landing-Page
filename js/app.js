da61e1f4d82d0890f4131a0fd0c291a939b1e9c3
Alex <failemotions95@gmail.com>
2019-09-16 23:13:03 +0300

    Accordion block was animated with js

--- js/app.js
+++ js/app.js
@@ -40,4 +40,25 @@ $(function() {
         }, 500);
     });
 
+
+
+   /* Menu nav toggle */
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+     /* Collapse */
+     $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        $this.toggleClass("active");
+    });
+
+
 });
